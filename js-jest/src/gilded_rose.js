e8f8d108c04b6c48ccc34724392697d76f3640b4
YuhWu <yuhwu@KILLERWHALE.zoo.lan>
2025-07-04 10:22:28 +0100

    comments for clarity

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -2,7 +2,7 @@ class Item {
   constructor(name, sellIn, quality){
     this.name = name;
     this.sellIn = sellIn;
-    this.quality = quality;
+    this.quality = quality; //bounded 0 and 50
   }
 }
 
@@ -11,22 +11,26 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
+    for (let i = 0; i < this.items.length; i++) { //for all items
+      //if not aged brie, backstage passes or sulfuras hand ragnaros, and quality is > 0, reduce qual by 1
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
+      } else { 
+        //if aged brie or backstage passes, and q < 50, q++.
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
+          //if backstage passes w sellin < 11 and q < 50, q ++
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+            //also if sellin < 6, q < 50, q++
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -35,9 +39,12 @@ class Shop {
           }
         }
       }
+
+      //anything not sulfuras sellin--
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      //if sellin < 0, and not aged brie or backstage passes ot sulfuras and q > 0 , q --
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -46,10 +53,10 @@ class Shop {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
+          } else { //if it is a backstage pass (not aged brie), q = 0
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
+        } else { //if aged brie, q < 50, q++
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
