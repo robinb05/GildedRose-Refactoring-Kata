e1ab60c1cd79903016a36402d729bcee7ddd34c6
Robin Blake <robinblake@ntlworld.com>
2025-07-04 11:44:58 +0100

    separated updates by item

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -40,47 +40,34 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) { //for all items
       //if not aged brie, backstage passes or sulfuras hand ragnaros, and quality is > 0, reduce qual by 1
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].quality = this.boundQuality(this.items[i].quality - 1);
-        }
-      } else { 
-        //if aged brie or backstage passes, and q < 50, q++.
+      if (this.items[i].name == 'Aged Brie') {
         this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
-        //if backstage passes w sellin < 11 and q < 50, q ++
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].sellIn < 11) {
-            this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
-          }
-          
-          //also if sellin < 6, q < 50, q++
-          if (this.items[i].sellIn < 6) {
-            this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
-          }
-    
-        }
-      }  
-
-      //anything not sulfuras sellin--
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      //if sellin < 0, and not aged brie or backstage passes ot sulfuras and q > 0 , q --
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              this.items[i].quality = this.boundQuality(this.items[i].quality - 1);
-            }
-          } else { //if it is a backstage pass (not aged brie), q = 0
-            this.items[i].quality = 0;
-          }
-        } else { //if aged brie, q < 50, q++
+        if (this.items[i].sellIn < 0) {
           this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
+        }
+      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
+        if (this.items[i].sellIn < 11) {
+            this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
+          }
+        if (this.items[i].sellIn < 6) {
+            this.items[i].quality = this.boundQuality(this.items[i].quality + 1);
+        }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = 0;
+        }
+      } else if (this.items[i].name == "Sulfuras, Hand of Ragnaros") {
+
+      } else {
+        this.items[i].quality = this.boundQuality(this.items[i].quality - 1);
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = this.boundQuality(this.items[i].quality - 1);
         }
       }
     }
-
     return this.items;
   }
 }
